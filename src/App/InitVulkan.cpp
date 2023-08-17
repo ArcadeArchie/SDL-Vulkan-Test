e75413015bd0f0fff929ca1c4b61cd084d72bf17
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-08-17 23:07:29 +0200

    cleaned up vulkan init code

--- src/App/InitVulkan.cpp
+++ src/App/InitVulkan.cpp
@@ -1,7 +1,6 @@
 #include "App.h"
 #include <vector>
 
-
 bool IsDeviceSuitable(VkPhysicalDevice device)
 {
     VkPhysicalDeviceProperties deviceProperties;
@@ -12,22 +11,53 @@ bool IsDeviceSuitable(VkPhysicalDevice device)
            deviceFeatures.geometryShader;
 }
 
-bool App::InitVulkan()
+VkApplicationInfo PopulateAppInfo(const char *appName)
 {
-    // create instance
     VkApplicationInfo app_info = {};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.apiVersion = VK_API_VERSION_1_3;
-    app_info.pApplicationName = windowName.c_str();
+    app_info.pApplicationName = appName;
     app_info.applicationVersion = VK_MAKE_API_VERSION(1, 0, 0, 0);
     app_info.pEngineName = "test-engine";
     app_info.engineVersion = VK_MAKE_API_VERSION(1, 0, 0, 0);
-
+    return app_info;
+}
+VkInstanceCreateInfo PopulateInstanceInfo(VkApplicationInfo *appInfo)
+{
     VkInstanceCreateInfo instance_info = {};
     instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instance_info.pApplicationInfo = &app_info;
+    instance_info.pApplicationInfo = appInfo;
     instance_info.enabledLayerCount = 0;
     instance_info.ppEnabledLayerNames = NULL;
+
+    return instance_info;
+}
+
+VkPhysicalDevice EnumerateDevices(VkInstance instance)
+{    
+    unsigned int deviceCount = 0;
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+    if (deviceCount == 0)
+    {
+        SDL_SetError("Failed to enumerate devices");
+        return VK_NULL_HANDLE;
+    }
+    for (const auto &device : devices)
+    {
+        if (IsDeviceSuitable(device))
+        {
+            return device;
+        }
+    }
+    return VK_NULL_HANDLE;
+}
+
+bool App::InitVulkan()
+{
+    // create instance
+    VkApplicationInfo app_info = PopulateAppInfo(windowName.c_str());
+    VkInstanceCreateInfo instance_info = PopulateInstanceInfo(&app_info);
 
     SDL_Vulkan_GetInstanceExtensions(window, &sdlExtCount, NULL);
     extNames = (const char **)malloc(sizeof(char *) * sdlExtCount);
@@ -46,28 +76,14 @@ bool App::InitVulkan()
 
     // enumerate devices
 
-    unsigned int deviceCount = 0;
-    std::vector<VkPhysicalDevice> devices(deviceCount);
-    vkEnumeratePhysicalDevices(vkInst, &deviceCount, devices.data());
-    if (deviceCount == 0)
-    {
-        SDL_SetError("Failed to enumerate devices");
-        return false;
-    }
-    VkPhysicalDevice physDevice;
-    for (const auto &device : devices)
-    {
-        if (IsDeviceSuitable(device))
-        {
-            physDevice = device;
-            break;
-        }
-    }
+    VkPhysicalDevice physDevice = EnumerateDevices(vkInst);
     if (physDevice == VK_NULL_HANDLE)
     {
         SDL_SetError("Failed to find suitable GPU");
         return false;
     }
+
+    
 
     return true;
 }
